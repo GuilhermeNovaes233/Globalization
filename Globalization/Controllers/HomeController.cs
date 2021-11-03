f4f61367485b517f983b22b5e5b75f4fa10e73c1
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-11-02 23:07:22 -0300

    Add project files.

---
+++ Globalization/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Globalization.Resources;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System;
+
+namespace Globalization.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult Get([FromServices] IStringLocalizer<Messages> localizer)
+        {
+            return Ok(new
+            {
+                Message = localizer["Hello World"].Value,
+                Date = DateTime.Now,
+                DateUtc = DateTime.Now.ToUniversalTime()
+            });
+        }
+    }
+}
